5fd8a99bf8f5a8d0c0fb0ca6387862b42b9ae04e
kameronyork <kamsyork@gmail.com>
2024-03-19 13:31:06 -0600

    changes to employee profile pictures

---
+++ test.py
@@ -0,0 +1,242 @@
+[
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "NOT STARTED ",
+        "assignedTo": "P - CRA POOL",
+        "assignedBy": "P - CRA POOL",
+        "assignedDateTime": "11/03/2023 08:57:00 AM",
+        "completedDateTime": "01/11/2024 04:51:15 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "P - CRA POOL",
+        "assignedDateTime": "01/11/2024 04:51:15 PM",
+        "completedDateTime": "02/01/2024 10:48:34 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Haroldsen, Hans",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/01/2024 10:48:34 PM",
+        "completedDateTime": "02/07/2024 04:04:20 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Story, Eric",
+        "assignedBy": "Haroldsen, Hans",
+        "assignedDateTime": "02/07/2024 04:04:20 PM",
+        "completedDateTime": "02/07/2024 04:04:20 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "P-SIMPLE PREP POOL",
+        "assignedBy": "Story, Eric",
+        "assignedDateTime": "02/07/2024 04:04:20 PM",
+        "completedDateTime": "02/07/2024 04:05:00 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Story, Eric",
+        "assignedBy": "P-SIMPLE PREP POOL",
+        "assignedDateTime": "02/07/2024 04:05:00 PM",
+        "completedDateTime": "02/07/2024 04:49:29 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Story, Eric",
+        "assignedDateTime": "02/07/2024 04:49:29 PM",
+        "completedDateTime": "02/07/2024 04:49:29 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Story, Eric",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/07/2024 04:49:29 PM",
+        "completedDateTime": "02/09/2024 08:46:04 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Story, Eric",
+        "assignedDateTime": "02/09/2024 08:46:04 PM",
+        "completedDateTime": "02/09/2024 08:46:04 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Haroldsen, Hans",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/09/2024 08:46:04 PM",
+        "completedDateTime": "02/12/2024 04:20:12 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Haroldsen, Hans",
+        "assignedDateTime": "02/12/2024 04:20:12 PM",
+        "completedDateTime": "02/12/2024 04:20:12 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Story, Eric",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/12/2024 04:20:12 PM",
+        "completedDateTime": "02/12/2024 06:37:16 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Haroldsen, Hans",
+        "assignedBy": "Story, Eric",
+        "assignedDateTime": "02/12/2024 06:37:16 PM",
+        "completedDateTime": "02/12/2024 06:54:50 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Haroldsen, Hans",
+        "assignedBy": "Haroldsen, Hans",
+        "assignedDateTime": "02/12/2024 06:54:50 PM",
+        "completedDateTime": "02/12/2024 06:55:44 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Haroldsen, Hans",
+        "assignedBy": "Haroldsen, Hans",
+        "assignedDateTime": "02/12/2024 06:55:44 PM",
+        "completedDateTime": "02/13/2024 06:08:08 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Haroldsen, Hans",
+        "assignedDateTime": "02/13/2024 06:08:08 PM",
+        "completedDateTime": "02/13/2024 09:27:21 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/13/2024 09:27:21 PM",
+        "completedDateTime": "02/13/2024 10:43:35 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/13/2024 10:43:35 PM",
+        "completedDateTime": "02/13/2024 11:11:49 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/13/2024 11:11:49 PM",
+        "completedDateTime": "02/13/2024 11:20:39 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/13/2024 11:20:39 PM",
+        "completedDateTime": "02/13/2024 11:26:53 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/13/2024 11:26:53 PM",
+        "completedDateTime": "02/13/2024 11:54:51 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/13/2024 11:54:51 PM",
+        "completedDateTime": "02/13/2024 11:59:52 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/13/2024 11:59:52 PM",
+        "completedDateTime": "02/15/2024 05:56:03 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/15/2024 05:56:03 PM",
+        "completedDateTime": "02/15/2024 06:50:21 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "REVIEW",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/15/2024 06:50:21 PM",
+        "completedDateTime": "02/15/2024 07:16:40 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/15/2024 07:16:40 PM",
+        "completedDateTime": "02/15/2024 07:25:37 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "PREPARATION",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "02/15/2024 07:25:37 PM",
+        "completedDateTime": "03/13/2024 02:15:54 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "SIGNER REVIEW",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "03/13/2024 02:15:54 PM",
+        "completedDateTime": "03/13/2024 03:39:54 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "SIGNER REVIEW",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "03/13/2024 03:39:54 PM",
+        "completedDateTime": "03/13/2024 03:55:17 PM"
+    },
+    {
+        "workFlowName": "TAX RETURN",
+        "step": "SIGNER REVIEW",
+        "assignedTo": "Arp, Jared",
+        "assignedBy": "Arp, Jared",
+        "assignedDateTime": "03/13/2024 03:55:17 PM",
+        "completedDateTime": null
+    }
+]
